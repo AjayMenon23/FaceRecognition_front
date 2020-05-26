96cf418581bf061279d5895c7106617ec793051c
Ajay Menon <ajayam23@gmail.com>
2020-05-26 11:14:32 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -86,7 +86,8 @@ class App extends Component{
 
 
   calculateFaceLocation =(data)=>{
-
+    const testing= data.outputs;
+    console.log(tesing);
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     console.log(clarifaiFace) ;
     const image = document.getElementById('inputImage');
